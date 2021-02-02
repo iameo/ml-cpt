1f8eccf9ae22998d112199cf6e950364710a1f04
iameo <okwudike.emmanuel@gmail.com>
2021-02-02 12:15:19 +0100

    example code lives and dies here

--- src/raw_code.py
+++ src/raw_code.py
@@ -8,4 +8,30 @@ def df_head():
     st.code('''df.head''', language='python)
 """
 
+#head
+def df_head():
+    st.code('''df.head()''', language='python')
 
+#shape
+def df_shape():
+    st.code('''df.shape''', language='python')
+
+#plot target
+def plot_target():
+    st.code("""
+                fig, ax = plt.subplots()
+                sns.countplot(data = train_data, x="target")
+                plt.show()
+
+""", language='python')
+
+
+def heatmap_code():
+    st.code('''
+    train_data.corr()["target"].sort_values(by="target", ascending=False)
+    ''', language='python')
+
+def heatmap_sns():
+    st.code('''
+    sns.heatmap(train.corr(), annot=True)
+    ''', language='python')
